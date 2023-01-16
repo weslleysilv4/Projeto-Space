b95b46906c9da54da12c4ac33c36132f70e174f0
Weslley Silva <weslleysilva.swe@gmail.com>
2023-01-16 12:57:39 -0300

    Commit Inicial

---
+++ js/main.js
@@ -0,0 +1,31 @@
+import NASA_API_KEY from "./API.js"
+
+const url = NASA_API_KEY;
+const btn = document.getElementById('btn-new');
+
+btn.addEventListener('click', async (e) =>{
+    e.preventDefault();
+    try {
+        const r = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${url}&count=1&hd=true`);
+        const jsonBody = await r.json();
+        
+        if(r.status === 200){
+            showResults({
+                date: jsonBody[0].date,
+                explanation: jsonBody[0].explanation,
+                title: jsonBody[0].title,
+                url: jsonBody[0].url,
+                hdurl: jsonBody[0].hdurl
+            })
+        }
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+function showResults(obj) {
+    document.querySelector('.gl-text').innerHTML = obj.explanation;
+    document.querySelector('.gl-tittle').innerHTML = obj.title;
+    document.querySelector('.gl-img').setAttribute('src',`${obj.url}`);
+    document.querySelector('.gl-img').setAttribute('srcset',`${obj.hdurl}`)
+};
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Projeto Space
+
+I made this project to study API consuming. I haved a lot of problems, but, i liked soo much to do this. Because, the problems are part of them and evolving in programming.
+
+Enjoy!!
